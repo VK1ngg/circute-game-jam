e13ea6680f1c4dc18086942d99fe03f512ce2e4e
VK1ngg <victor.a.m.aires@gmail.com>
2026-08-30 17:19:45 -0300

    Pause Menu I

---
+++ scripts/teste.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var pause_menu = $Pause
+var paused = false
+
+func _on_pause_button_pressed() -> void:
+	pauseMenu()
+
+func pauseMenu():
+	if paused == false:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	paused = !paused
