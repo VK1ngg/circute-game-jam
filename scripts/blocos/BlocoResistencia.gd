828090e11d60f47cb7e65f6cee8b522398f1dee4
VK1ngg <victor.a.m.aires@gmail.com>
2026-08-30 11:37:14 -0300

    Energia VI

--- scripts/blocos/BlocoResistencia.gd
+++ scripts/blocos/BlocoResistencia.gd
@@ -1,15 +1,10 @@
 extends Area2D
 
-var energy = false
-var line: String = ''
+var energia = 0
 @onready var sprite = $_
 
 func energizar(body: Node2D) -> void:
-	if energy == true: body.energy = true
+	if energia >= 1: body.energia += 1
 
 func _process(_delta: float) -> void:
-	if energy == true: $AnimationPlayer.play("energizado")
-
-func _on_mouse_entered() -> void: sprite.scale = Vector2(1.05, 1.05)
-
-func _on_mouse_exited() -> void: sprite.scale = Vector2(1, 1)
+	if energia >= 1: $AnimationPlayer.play("energizado")
