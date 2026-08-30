fc1cb3eac897411132355973f2e6e8dba936279a
Rennan555 <dungeonbr5@gmail.com>
2026-08-30 10:07:52 -0300

    fix: Energy variable name fixed

--- scripts/blocos/BlocoCapacitor.gd
+++ scripts/blocos/BlocoCapacitor.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-var energia = false
+var energy = false
 var line: String = ''
 @onready var sprite = $_
 
 func energizar(body: Node2D) -> void:
-	if energia == true: body.energia = true
+	if energy == true: body.energy = true
 
 func _process(_delta: float) -> void:
-	if energia == true: $AnimationPlayer.play("energizado")
+	if energy == true: $AnimationPlayer.play("energizado")
 
 func _on_mouse_entered() -> void: sprite.scale = Vector2(1.05, 1.05)
 
